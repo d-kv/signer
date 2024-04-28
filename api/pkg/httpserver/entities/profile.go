1d88dba4775947d2ec59d45fac2086a639ff95d3
Matthew Wideman <74961598+themotka@users.noreply.github.com>
2024-04-28 09:35:16 +0300

    deleted unused entities

--- api/pkg/httpserver/entities/profile.go
+++
@@ -1,8 +0,0 @@
-package entities
-
-type Profile struct {
-	Identifier    string `json:"identifier"`
-	Name          string `json:"name"`
-	BundleId      string `json:"bundleId"`
-	IntegrationId string `json:"integrationId"`
-}
