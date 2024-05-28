b0e94fb82e410401e47d7b9c31d1a02b37fddb82
maksmolch <maksmolchdmitr@gmail.com>
2024-05-28 13:09:21 +0300

    add handlers impl

--- query-handler/pkg/httpserver/handlers/certificates.go
+++ query-handler/pkg/httpserver/handlers/certificates.go
@@ -1,10 +1,33 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"d-kv/signer/query-handler/pkg/httpserver/entities"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func (h *Handler) getCertificates(c *gin.Context) {
+	integrationId := c.Param("integrationId")
+	certificate, err := h.DomainRepos.CertificateRepo.FindByIntegrationId(c.Request.Context(), integrationId)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	c.JSON(http.StatusOK, entities.Certificate{
+		ID:                 certificate.ID,
+		CertificateContent: certificate.CertificateContent,
+	})
 }
 
 func (h *Handler) getCertificateByID(c *gin.Context) {
-	///TODO Maxim, implement me
+	id := c.Param("id")
+	certificate, err := h.DomainRepos.CertificateRepo.FindById(c.Request.Context(), id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	c.JSON(http.StatusOK, entities.Certificate{
+		ID:                 certificate.ID,
+		CertificateContent: certificate.CertificateContent,
+	})
 }
